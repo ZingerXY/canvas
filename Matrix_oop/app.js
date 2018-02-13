f8f83bc49d45a5ec8bfd1e12143251585fee1e29
ZingerXY <Vitaliks90@mail.ru>
2018-02-13 21:08:14 +0300

    Обновление до актуальной версии

--- Matrix_oop/app.js
+++ Matrix_oop/app.js
@@ -1,15 +1,17 @@
-//function Matrix(){function n(n,t){return Math.floor(Math.random()*(t-n+1))+n}function t(n,t,e){return n>=e?e:t>n?t:n}function e(t,e){var o=n(t,e);return 20>o?n(l[f].mn,l[f].mx):o>=20&&30>=o?n(l[f].mnn,l[f].mxx):o>30?32:void 0}function o(){x.clearRect(0,0,window.innerWidth,window.innerHeight),s.width=window.innerWidth,s.height=window.innerHeight,x.font="18px Consolas, Lucida Console, monospace";var n=Math.floor(window.innerWidth/x.measureText(l[f].ch).width);d=[];for(var o=[],c=0;n>c;c++)" "!=a[c]?d[c]=String.fromCharCode(e(0,i)):d[c]=String.fromCharCode(e(r,100)),a[c]=d[c],o[c]=0;h[0]=d;for(var w=Math.floor(window.innerHeight/18),c=w+12;0!=c;c--)h[c]=h[c-1]?h[c-1]:[];for(var c=w+12;0!=c;c--)for(var u in h[c]){" "!=h[c][u]?o[u]++:o[u]=0;var g,v=[50,200,50];g=m?"rgba("+t(v[0]-10*o[u],7,255)+", "+t(v[1]-10*o[u],7,255)+", "+t(v[2]-10*o[u],7,255)+", 1)":"rgba(50, 200, 50, "+t(1-.05*o[u],0,1)+")",1==o[u]&&(g="rgba(85, 255, 85, 1)"),x.fillStyle=g,x.shadowColor=g,x.shadowBlur=5,w>=c&&x.fillText(h[c][u],u*x.measureText(l[f].ch).width,18*c)}}var r=29,i=35,d=[],a=[],h=[],f=0,l=[{ch:"0",mn:48,mx:57,mnn:59,mxx:126},{ch:String.fromCharCode(12448),mn:12448,mx:12543,mnn:12448,mxx:12543}],m=!1,c=0,w=0,s=document.createElement("canvas");s.style.position="fixed",s.style.top="0px",s.style.left="0px",s.style.zIndex=w,s.width=window.innerWidth,s.height=window.innerHeight,document.body.insertBefore(s,document.body.firstChild);var x=s.getContext("2d");document.onkeydown=function(n){90===n.keyCode&&(m=m?!1:!0),88===n.keyCode&&(w=w?0:9999),s.style.zIndex=w},this.Stop=function(){clearInterval(c),c=0},this.Start=function(){c||setInterval(o,100)},this.Start()}var matrix=new Matrix;
+//function Matrix(){var t=29,n=35,e=[],o=[],i=[],r=!1,d=!0,l=!0,a=0,h=0,f="0123456789ƎƏƐƑƒƓƔƕƖƗƘƙƚƛƜƝƞƟƠơƢƣƤƥƦƧƨƩƪƫƬƭƮƯưƱƲɐɑɒɓɔɕɖɗɘəɚɛɜɝɞɟɠɡɢɣɤɥɦɧɨɯɰɱɲɳɴɵɶʙʚʛʜʞαβγδεζηθικλμνξοπρςστυφχψωϧϨϩϪϫϬϭϮϯϰϱϲ",s=[50,200,50],u=[55,55,55],w=document.createElement("canvas");w.style.position="fixed",w.style.top="0px",w.style.left="0px",w.style.zIndex=h,w.width=window.innerWidth,w.height=window.innerHeight,document.body.insertBefore(w,document.body.firstChild);var c=w.getContext("2d");function g(t,n){return Math.floor(Math.random()*(n-t+1))+t}function y(t,n,e){return t>=e?e:t<n?n:t}function v(t,n){var e=g(t,n);return e<20?f[g(0,f.length-1)]:e>=20&&e<=30?f[g(0,f.length-1)]:e>30?" ":void 0}function x(){if(l){w.width=window.innerWidth,w.height=window.innerHeight,w.style.zIndex=h,c.clearRect(0,0,window.innerWidth,window.innerHeight),c.font="18px Consolas, Lucida Console, monospace";var a=Math.floor(window.innerWidth/c.measureText(f[0]).width);e=[];for(var x=[],C=0;C<a;C++)" "!=o[C]?e[C]=v(0,n):e[C]=v(t,100),o[C]=e[C],x[C]=0;i[0]=e;var m,p,k,b,H,I=Math.floor(window.innerHeight/18);for(C=I+12;0!=C;C--)i[C]=i[C-1]?i[C-1]:[];d&&(m=s[0],p=s[1],k=s[2],H=50,m==(b=200)?k==b?m--:p==H&&k>=H?k++:k==H&&p>H&&p--:p==b?m==b?p--:k==H&&m>=H?m++:m==H&&k>H&&k--:k==b&&(p==b?k--:m==H&&p>=H?p++:p==H&&m>H&&m--),s=[m,p,k]);for(C=I+12;0!=C;C--)for(var M in i[C]){var S;" "!=i[C][M]?(i[C][M]=f[g(0,f.length-1)],x[M]++):x[M]=0,S=r?"rgba("+y(s[0]-10*x[M],7,255)+", "+y(s[1]-10*x[M],7,255)+", "+y(s[2]-10*x[M],7,255)+", 1)":"rgba("+s[0]+", "+s[1]+", "+s[2]+", "+y(1-.05*x[M],0,1)+")",1==x[M]&&(S="rgba("+y(s[0]+u[0],0,255)+", "+y(s[1]+u[1],0,255)+", "+y(s[2]+u[2],0,255)+", 1)"),c.fillStyle=S,c.shadowColor=S,c.shadowBlur=5,C<=I&&c.fillText(i[C][M],M*c.measureText(f[0]).width,18*C)}}}document.onkeydown=function(t){console.log(t.keyCode),90===t.keyCode&&(r=!r),88===t.keyCode&&(h=h?0:9999,w.style.zIndex=h),67===t.keyCode&&(d=!d),86===t.keyCode&&(l=!l)},this.Stop=function(){clearInterval(a),a=0},this.Start=function(){a||(a=setInterval(x,100))},this.ColorH=function(t,n,e){if(!arguments.length)return s;s=[t,n,e]},this.ColorP=function(t){if(!arguments.length)return u[0];u=[t,t,t]},this.Start()}var matrix = new Matrix;
 function Matrix() {
 	var minn = 29, maxx = 35;
 	var a = [];
 	var b = [];
 	var obj = [];
-	var num = 0;
-	var set = [	{ch:'0',mn:48,mx:57,mnn:59,mxx:126},
-				{ch:String.fromCharCode(0x30A0),mn:0x30A0,mx:0x30FF,mnn:0x30A0,mxx:0x30FF}];
 	var test = false;
+	var cp = true;
+	var on = true;
 	var timer = 0;
 	var zInd = 0;
+	var arrChar = "0123456789ƎƏƐƑƒƓƔƕƖƗƘƙƚƛƜƝƞƟƠơƢƣƤƥƦƧƨƩƪƫƬƭƮƯưƱƲɐɑɒɓɔɕɖɗɘəɚɛɜɝɞɟɠɡɢɣɤɥɦɧɨɯɰɱɲɳɴɵɶʙʚʛʜʞαβγδεζηθικλμνξοπρςστυφχψωϧϨϩϪϫϬϭϮϯϰϱϲ"	
+	var h = [50,200,50];
+	var p = [55,55,55];
 	
 	var canvas = document.createElement('canvas');
 	canvas.style.position = "fixed";
@@ -20,7 +22,13 @@ function Matrix() {
 	canvas.height = window.innerHeight;
 	document.body.insertBefore(canvas, document.body.firstChild);
 	var ctx = canvas.getContext('2d');
-	document.onkeydown = function(e){ if(e.keyCode === 90) test = test ? false : true;if(e.keyCode === 88) zInd = zInd ? 0 : 9999; canvas.style.zIndex = zInd;};
+	document.onkeydown = function(e){ 
+		console.log(e.keyCode);
+		if(e.keyCode === 90) {test = !test;} // z немного меняет отображение хвостов
+		if(e.keyCode === 88) {zInd = zInd ? 0 : 9999; canvas.style.zIndex = zInd;} // x меняет положение матрицы передний/задний план
+		if(e.keyCode === 67) {cp = !cp;} // с вкл/выкл изменение цвета
+		if(e.keyCode === 86) {on = !on;} // v вкл/выкл анимацию
+	};
 	
 	this.Stop = function() {
 		clearInterval(timer);
@@ -29,7 +37,38 @@ function Matrix() {
 	
 	this.Start = function() {
 		if(!timer)
-			setInterval(nextRound, 100);
+			timer = setInterval(nextRound, 100);
+	}
+	
+	this.ColorH = function(r,g,b) {
+		if (!arguments.length) return h;
+		h = [r,g,b];
+	}
+	this.ColorP = function(l) {
+		if (!arguments.length) return p[0];
+		p = [l,l,l];
+	}
+	
+	/*function colors(r,g,b) {
+		var max = 200, min = 50;
+		if(r == max)
+			if(b == max)r--;
+			else if(g==min&&b>=min) b++;
+			else if (b==min&&g>min)	g--;
+		else if(g == max) 
+			if(r == max) g--;
+			else if(b==min&&r>=min) r++;
+			else if (r==min&&b>min)	b--;
+		else if(b == max) 
+			if(g == max) b--;
+			else if(r==min&&g>=min) g++;
+			else if (g==min&&r>min)	r--;
+		return [r,g,b];
+	} -->	*/
+	function colors(r,g,b){
+		var d=200,e=50;
+		r==d?b==d?r--:g==e&&b>=e?b++:b==e&&g>e&&g--:g==d?r==d?g--:b==e&&r>=e?r++:r==e&&b>e&&b--:b==d&&(g==d?b--:r==e&&g>=e?g++:g==e&&r>e&&r--);
+		return [r,g,b];
 	}
 
 	function Rand(min, max) {
@@ -40,48 +79,53 @@ function Matrix() {
 	}
 	function randi(n, m) {
 		var p = Rand(n, m);
-		if(p < 20) return Rand(set[num].mn, set[num].mx);
-		if(p >= 20 && p <= 30) return Rand(set[num].mnn, set[num].mxx);
-		if(p > 30) return 32;
+		if(p < 20) return arrChar[Rand(0, arrChar.length-1)];
+		if(p >= 20 && p <= 30) return arrChar[Rand(0, arrChar.length-1)];
+		if(p > 30) return ' ';
 	}
 	// Перерисовка
 	function nextRound() {
-		ctx.clearRect(0,0,window.innerWidth,window.innerHeight);
+		if(!on) return;		
 		canvas.width = window.innerWidth;
 		canvas.height = window.innerHeight;
+		canvas.style.zIndex = zInd;		
+		ctx.clearRect(0,0,window.innerWidth,window.innerHeight);
 		ctx.font = "18px Consolas, Lucida Console, monospace";
-		var l = Math.floor(window.innerWidth/ctx.measureText(set[num].ch).width);
+		var l = Math.floor(window.innerWidth/ctx.measureText(arrChar[0]).width);
 		a = [];
 		var lan = [];
 		for(var i = 0; i < l; i++) {
-			if(b[i] != ' ') a[i] = String.fromCharCode(randi(0, maxx));		
-			else a[i] = String.fromCharCode(randi(minn, 100));
+			if(b[i] != ' ') a[i] = randi(0, maxx);		
+			else a[i] = randi(minn, 100);
 			b[i] = a[i];
 			lan[i] = 0;
 		}
 		obj[0] = a;
 		var start = Math.floor(window.innerHeight/18);	
 		for(var i = start+12; i != 0; i--)
-			obj[i] = obj[i-1] ? obj[i-1] : []; 
+			obj[i] = obj[i-1] ? obj[i-1] : [];
+		if(cp)
+			h = colors(h[0],h[1],h[2]);
 		for(var i = start+12; i != 0; i--)
 			for(var j in obj[i]) {
-				if(obj[i][j] != ' ') 
+				if(obj[i][j] != ' '){
+					obj[i][j] = arrChar[Rand(0, arrChar.length-1)];
 					lan[j]++;
+				}
 				else 
 					lan[j] = 0;
-				var c = [50,200,50];
 				var color;
 				if(test)
-					color = "rgba("+clamp(c[0]-10*lan[j],7,255)+", "+clamp(c[1]-10*lan[j],7,255)+", "+clamp(c[2]-10*lan[j],7,255)+", 1)";
+					color = "rgba("+clamp(h[0]-10*lan[j],7,255)+", "+clamp(h[1]-10*lan[j],7,255)+", "+clamp(h[2]-10*lan[j],7,255)+", 1)";
 				else
-					color = "rgba(50, 200, 50, "+clamp(1-0.05*lan[j],0,1)+")";
+					color = "rgba("+h[0]+", "+h[1]+", "+h[2]+", "+clamp(1-0.05*lan[j],0,1)+")";
 				if(lan[j] == 1)
-					color = "rgba(85, 255, 85, 1)"
+					color = "rgba("+clamp(h[0]+p[0],0,255)+", "+clamp(h[1]+p[1],0,255)+", "+clamp(h[2]+p[2],0,255)+", 1)"
 				ctx.fillStyle = color;
 				ctx.shadowColor = color;
 				ctx.shadowBlur = 5;
 				if(i <= start)
-					ctx.fillText(obj[i][j], j*ctx.measureText(set[num].ch).width, 18*i);
+					ctx.fillText(obj[i][j], j*ctx.measureText(arrChar[0]).width, 18*i);
 			}
 	}
 	this.Start();
